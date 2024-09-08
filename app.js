dccfcd1d47a88fee9e376a997b921833be7d74d1
Dileepar26 <dilee.ram26@gmail.com>
2024-09-08 12:52:04 +0530

    dotenv

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const { requireAuth } = require('./middleware/authMiddleware');
 
@@ -15,7 +14,6 @@ app.use(express.json());
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies',requireAuth, (req, res) => res.render('smoothies'));
-console.log(process.env.data)
 app .use(require('./routes/authRoutes'))
 port = 5000
 app.listen(port,()=>console.log(`server is running at port number ${port}`))
